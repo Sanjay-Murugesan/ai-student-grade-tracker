1032ce0aa1140c431e5188d08228022e38049a60
Sanjay M <mssanjay4444@gmail.com>
2026-04-11 17:01:48 +0530

    Clean up additional build artifacts and duplicate files

--- backend/backend/src/main/java/com/studenttracker/backend/repository/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.studenttracker.backend.repository;
-
-import com.studenttracker.backend.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {}
